2acad96cf22989be5f1e5ab7372a489aa4a58f6d
Asrulhasfazuhiram Ramli <lursa17@hotmail.com>
2017-06-05 04:26:13 +0000

    Pushed from Dropbox

--- includes/braintree_init.php
+++ includes/braintree_init.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once("../lib/autoload.php");
+require_once("../vendor/autoload.php");
 
 if(file_exists(__DIR__ . "/../.env")) {
     $dotenv = new Dotenv\Dotenv(__DIR__ . "/../");
