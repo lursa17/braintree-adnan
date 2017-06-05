8882dbf8b12c6ee85e183c215b5c34d527353254
Asrulhasfazuhiram Ramli <lursa17@hotmail.com>
2017-06-05 06:34:43 +0000

    Pushed from Dropbox

---
+++ public_html/_environment.php
@@ -0,0 +1,11 @@
+<?php
+
+    require_once('../vendor/braintree/braintree-php/lib/Braintree.php');
+    Braintree_Configuration::environment('sandbox');
+    Braintree_Configuration::merchantId('pf2gf49pptqb6dcv');
+    Braintree_Configuration::publicKey('nrzghfkjqw3tb3vw');
+    Braintree_Configuration::privateKey('b269102882ac39f551b98d5235c74b3a');
+
+
+
+?>
